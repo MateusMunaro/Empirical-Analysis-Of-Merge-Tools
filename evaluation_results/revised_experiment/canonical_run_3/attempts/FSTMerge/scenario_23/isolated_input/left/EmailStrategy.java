92572136d4ce81378000d2b5a9ab1315270275ec
Mateus Augusto Munaro de Souza <mateusmunaro40@gmail.com>
2026-08-22 16:45:15 -0300

    chore: reset generated evaluation artifacts

--- evaluation_results/revised_experiment/canonical_run_3/attempts/FSTMerge/scenario_23/isolated_input/left/EmailStrategy.java
+++
@@ -1,8 +0,0 @@
-public class EmailStrategy implements NotificationStrategy {
-    @Override
-    public void send(String message, String recipient) {
-        System.out.println("Enviando email para: " + recipient);
-        System.out.println("Conteúdo: " + message);
-        System.out.println("Email enviado com sucesso!");
-    }
-}
--- evaluation_results/revised_experiment/canonical_run_4_accept_non_java/attempts/FSTMerge/scenario_35/isolated_input/base/User.java
+++
@@ -1,8 +0,0 @@
-public class User {
-    private String username;
-    private int age;
-    
-    public void setAge(int age) {
-        this.age = age;
-    }
-}
